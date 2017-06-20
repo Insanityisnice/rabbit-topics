2c94a1fb7e27b46aa402cc50d2f11cdcf4962e1b
James Stackhouse <insanityisnice@hotmail.com>
2017-06-20 13:18:48 -0700

    Create the producer api.

---
+++ producer/Controllers/MessagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace producer.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Success!";
+        }
+
+        // POST api/messages
+        [HttpPost]
+        [Route("{exchange}/{queue}/{routeKey}")]
+        public string Post(string exchange, string queue, string routeKey, [FromBody]string message)
+        {
+            return $"{exchange}:{queue}:{routeKey} - Success!";
+        }
+    }
+}
---
+++ producer/readme.md
@@ -0,0 +1,5 @@
+# build
+docker build -t producer .
+
+# run
+dotnet run -it -rm producer
