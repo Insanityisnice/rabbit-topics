9a0d972b54daa839296054cb0676234a6b296c6d
Insanityisnice <insanityisnice@hotmail.com>
2017-06-26 08:30:12 -0700

    Consumer building.

---
+++ consumer/Controllers/ConsumersController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using consumer.Services;
+using consumer.Models;
+
+namespace consumer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConsumersController : Controller
+    {
+        private ILogger logger;
+        private ConsumerService consumerService;
+
+        public ConsumersController(ILoggerFactory loggerFactory, ConsumerService consumerService)
+        {
+            this.logger = loggerFactory.CreateLogger(nameof(ConsumersController));
+            this.consumerService = consumerService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return consumerService.Consumers().Select(c => new
+                { 
+                    Name = c.Name,
+                    Exchange = c.Exchange,
+                    BindingKey = c.BindingKey,
+                });
+        }
+
+         [HttpPost]
+        public object Post([FromBody] Consumer consumer)
+        {
+            logger.LogInformation($"Adding new consumer {consumer.Name}.");
+
+            if(consumer == null)
+            {
+                return BadRequest();
+            }
+
+            consumerService.AddConsumer(consumer);
+            return new {
+                Name = consumer.Name,
+                Exchange = consumer.Exchange,
+                BindingKey = consumer.BindingKey
+            };
+        }
+    }
+}
--- consumer/Controllers/MessagesController.cs
+++ consumer/Controllers/MessagesController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using consumer.Services;
+using consumer.Models;
 
 namespace consumer.Controllers
 {
-    [Route("api/consumer/[consumerName][controller]")]
+    [Route("api/consumers/[consumerName]/[controller]")]
     public class MessagesController : Controller
     {
+        private ILogger logger;
+        private ConsumerService consumerService;
+
+        public MessagesController(ILoggerFactory loggerFactory, ConsumerService consumerService)
+        {
+            this.logger = loggerFactory.CreateLogger(nameof(MessagesController));
+            this.consumerService = consumerService;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get(string consumerName)
         {
             if(string.IsNullOrWhiteSpace(consumerName)) throw new ArgumentException($"The argument {nameof(consumerName)} is required.", nameof(consumerName));
-            
-            return ConsumerService.Instance[consumerName].Messages;
+
+            return consumerService[consumerName].Messages;
         }
     }
 }
